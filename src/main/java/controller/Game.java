3f4f37f31a52a96fbf635ac21b531c85a3ab1c2d
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-02-27 10:27:39 +0100

    Add optionPvP and PvAi

--- src/main/java/controller/Game.java
+++ src/main/java/controller/Game.java
@@ -3,26 +3,27 @@ package controller;
 import exception.RandomizeDeckException;
 import services.TerminalManager;
 
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Game {
     Table table;
 
-    public Game() throws RandomizeDeckException {
+    public Game() throws RandomizeDeckException, FileNotFoundException {
         init();
     }
 
 
-    public void optionPvP() throws RandomizeDeckException {
+    public void optionPvP() throws RandomizeDeckException, FileNotFoundException {
 //        player1 = new PlayerHuman("Andrzej");
 //        player2 = new PlayerHuman("Stefan");
 //      TODO:
-        System.out.println("playPVP");
+        System.out.println("optionPVP");
         table = new Table(2);
 
     }
 
-    public void optionPvAi() throws RandomizeDeckException {
+    public void optionPvAi() throws RandomizeDeckException, FileNotFoundException {
 //        player1 = new PlayerHuman("Andrzej");
 //        ai = new PlayerAI();
 //      TODO:
@@ -34,7 +35,7 @@ public class Game {
 
     }
 
-    public void init() throws RandomizeDeckException {
+    public void init() throws RandomizeDeckException, FileNotFoundException {
         boolean isRunning = true;
         System.out.println("play()");
 
--- src/main/java/controller/Table.java
+++ src/main/java/controller/Table.java
@@ -2,8 +2,10 @@ package controller;
 
 import exception.RandomizeDeckException;
 import model.Player;
+import model.PlayerAI;
 import model.PlayerHuman;
 
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Table {
@@ -13,7 +15,7 @@ public class Table {
     Player playerAi;
 
 
-    public Table(int numberOfPlayers) throws RandomizeDeckException {
+    public Table(int numberOfPlayers) throws RandomizeDeckException, FileNotFoundException {
         System.out.println("Welcome on the table");
 
         if (numberOfPlayers > 1) {
@@ -24,22 +26,39 @@ public class Table {
 
     }
 
-    private void playGame(int numberOfPlayers) throws RandomizeDeckException {
+    private void playGame(int numberOfPlayers) throws RandomizeDeckException, FileNotFoundException {
         if (numberOfPlayers > 1) {
+            System.out.println("What is the name of the Player 1");
             String nameOfPlayer1 = scanner.nextLine();
             player1 = new PlayerHuman(nameOfPlayer1);
 
+            System.out.println("What is the name of the Player 2");
             String nameOfPlayer2 = scanner.nextLine();
             player2 = new PlayerHuman(nameOfPlayer2);
+
             playPvP(player1, player2);
+        } else {
+            String nameOfPlayer1 = scanner.nextLine();
+            player1 = new PlayerHuman(nameOfPlayer1);
+            playerAi = new PlayerAI();
+
+            playPvAi(player1, playerAi);
         }
     }
 
-    private void playPvP(Player player1, Player player2) {
+    private void playPvP(Player player1, Player player2) throws FileNotFoundException {
         while (isPlayerAlive(player1) || isPlayerAlive(player2)) {
-
+            System.out.println("playPvP");
+            player1.attack(player2);
+            player2.attack(player1);
         }
+    }
 
+    private void playPvAi(Player player1, Player playerAi) throws FileNotFoundException {
+        while (isPlayerAlive(player1) || isPlayerAlive(playerAi)) {
+            player1.attack(player2);
+            player2.attack(player1);
+        }
     }
 
     private boolean isPlayerAlive(Player playerToLive) {
