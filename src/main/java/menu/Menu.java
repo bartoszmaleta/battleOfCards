1fb2f2a231497d2814c2d6e6459e24e671a51156
codewarior123 <przemek.buszek@onet.eu>
2020-02-27 10:19:55 +0100

    small fixes menu, added graphic

--- src/main/java/menu/Menu.java
+++ src/main/java/menu/Menu.java
@@ -11,8 +11,9 @@ public class Menu {
         System.out.println("\033[0;37;49mMenu");
         System.out.println("Choose option:");
 
-        System.out.println("1. Play Game with AI");
-        System.out.println("2. Exit program");
+        System.out.println("a. Play Game with easy AI");
+        System.out.println("b. Play game with hard AI");
+        System.out.println("c. Exit program");
 
         MenuFunctions.emptyLinesThree();
     }
@@ -29,10 +30,12 @@ public class Menu {
             String choosedOption = scanner.nextLine();
 
             switch (choosedOption) {
-            case "1":
+            case "a":
                 isRunning = false;
                 break;
-            case "2":
+            case "b":
+                isRunning = false;
+            case "c":
                 System.exit(0);
                 break;
             default:
