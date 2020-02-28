5a181c9c85c7a45bdb0019a866b6a6f8eb526353
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-02-28 01:22:57 +0100

    Add todos

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,25 +4,28 @@ import java.io.FileNotFoundException;
 
 // Remember to change path of "Cards.xml"
 // TODO:
-//       - presentation: projectStructure from IDE
-//       - more graphics
-//       - healthCalculating loadingBar
-//       - clearScreen()
-//       - allignment
-//       - experience dependency
-//       - level dependency
-//       - coins dependency
-//       - coins deposit after draw
-//       - better newLines()
-//       - menu during game
-//       - attack() and bet() break into smaller methods
-//       - calculateHealth() move to one method = "update"
-//       - show name of Smurf on his card image
-//       - change some characters to emoji
-//       - add more colours
-//       - high scores
-//       - emoji of the player
+//       - more graphics                                                COOL FEATURE
+//       - healthCalculating loadingBar                                 COOL FEATURE
+//       - clearScreen()                                                HARD IN INTELLIJ
+//       - allignment                                                   COOL FEATURE
+//       - experience dependency                                        MUST HAVE
+//       - level dependency                                             MUST HAVE
+//       - coins dependency                                             MUST HAVE
+//       - coins deposit after draw                                     MUST HAVE
+//       - better newLines()                                            HARD IN INTELLIJ
+//       - menu during game                                             MUST HAVE
+//       - attack() and bet() break into smaller methods                FUTURE
+//       - calculateHealth() move to one method = "update"              FUTURE
+//       - show name of Smurf on his card image                         MUST HAVE
+//       - change some characters to emoji                              DONE (NOT IN INTELLIJ)
+//       - add more colours                                             DONE
+//       - high scores                                                  FUTURE
+//       - emoji of the player                                          DONE
 //       -
+//       -
+//       - presentation: projectStructure from IDE, github network, uml, todos on basecamp
+//                       , images of cardDisplay and stats, XML example, img todos in Main Class
+//                       , github branches,
 
 public class Main {
     public static void main(String[] args) throws RandomizeDeckException, FileNotFoundException {
